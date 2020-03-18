25da3291f835a5a8b5d616ae65b98063e42a787a
essam993 <ahm.essam993@gmail.com>
2020-03-19 00:35:25 +0200

    text

---
+++ app/etc/env.php
@@ -0,0 +1,74 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_q5we0l'
+    ],
+    'crypt' => [
+        'key' => '159c4e8771bf47e1039e92f8ca06abac'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'itambiti_clothes',
+                'username' => 'itambiti_clothes',
+                'password' => '8DruC}yGYnCU',
+                'active' => '1',
+                'driver_options' => [
+
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '110_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '110_'
+            ]
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => null
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'google_product' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'downloadable_domains' => [
+        'clothes.itambition.com'
+    ],
+    'install' => [
+        'date' => 'Sun, 01 Mar 2020 09:39:47 +0000'
+    ]
+];
